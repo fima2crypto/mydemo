1c70c2469978ff7b246b6034be021839a01981ea
fima2crypto <100105217+fima2crypto@users.noreply.github.com>
2026-05-02 17:15:38 +0700

    Your commit message

--- lib/kenoService.ts
+++ lib/kenoService.ts
@@ -55,17 +55,24 @@ const TAB_KEYS = [
   "chon_2",
   "chon_1",
 ];
+
 const HEADERS = {
   "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-  Accept: "application/json, text/plain, */*",
-  "Accept-Language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+  Accept:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+  "Accept-Language": "vi-VN,vi;q=0.9,en-US;q=0.8,en;q=0.7",
   Referer: "https://vietlott.vn",
-  Origin: "https://vietlott.vn",
-  "Cache-Control": "no-cache",
-  Pragma: "no-cache",
+  "Sec-Ch-Ua":
+    '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+  "Sec-Ch-Ua-Mobile": "?0",
+  "Sec-Ch-Ua-Platform": '"Windows"',
+  "Sec-Fetch-Dest": "document",
+  "Sec-Fetch-Mode": "navigate",
+  "Sec-Fetch-Site": "same-origin",
+  "Sec-Fetch-User": "?1",
+  "Upgrade-Insecure-Requests": "1",
 };
-
 // const HEADERS = {
 //   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
 //   Referer: `${BASE_URL}/vi/trung-thuong/ket-qua-trung-thuong/winning-number-keno`,
